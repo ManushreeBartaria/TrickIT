d3cbfdccffb67e7da795985b65a8794255890d70
unknown <manushreebartaria07@gmail.com>
2026-03-18 17:21:24 +0530

    from host.minikube.internal to localhost conf changed

--- backend/app/database/connections.py
+++ backend/app/database/connections.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@host.minikube.internal:3306/trickit_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/trickit_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
